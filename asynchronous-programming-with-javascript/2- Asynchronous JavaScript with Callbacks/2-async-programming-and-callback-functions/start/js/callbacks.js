f6d7e1ef39a984085a7d7d7a689dcc295263709e
Noman Rafiq <heyimnomee@gmail.com>
2024-01-09 17:35:14 +0500

    astronauts

--- asynchronous-programming-with-javascript/2- Asynchronous JavaScript with Callbacks/2-async-programming-and-callback-functions/start/js/callbacks.js
+++ asynchronous-programming-with-javascript/2- Asynchronous JavaScript with Callbacks/2-async-programming-and-callback-functions/start/js/callbacks.js
@@ -4,36 +4,46 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Make an AJAX request
-function getJSON(url) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = () => {
-    if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      console.log(data);
-    }
-  };
-  xhr.send();
+function getJSON(url, cb) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            let data = JSON.parse(xhr.responseText);
+            console.log(data);
+            return cb(data);
+        }
+    };
+    xhr.send();
 }
 
 // Generate the markup for each profile
 function generateHTML(data) {
-  const section = document.createElement('section');
-  peopleList.appendChild(section);
-  // Check if request returns a 'standard' page from Wiki
-  if (data.type === 'standard') {
-    section.innerHTML = `
+    const section = document.createElement('section');
+    peopleList.appendChild(section);
+    // Check if request returns a 'standard' page from Wiki
+    if (data.type === 'standard') {
+        section.innerHTML = `
       <img src=${data.thumbnail.source}>
       <h2>${data.title}</h2>
       <p>${data.description}</p>
       <p>${data.extract}</p>
     `;
-  } else {
-    section.innerHTML = `
+    } else {
+        section.innerHTML = `
       <img src="img/profile.jpg" alt="ocean clouds seen from space">
       <h2>${data.title}</h2>
       <p>Results unavailable for ${data.title}</p>
       ${data.extract_html}
     `;
-  }
+    }
 }
+
+btn.addEventListener('click', () => {
+    getJSON(astrosUrl, (json) => {
+        console.log(json.people);
+        json.people.map((person) => {
+            getJSON(wikiUrl + person.name, generateHTML);
+        });
+    });
+});
