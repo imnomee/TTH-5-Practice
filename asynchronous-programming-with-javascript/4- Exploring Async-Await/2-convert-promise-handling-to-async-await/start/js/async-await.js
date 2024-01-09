8068e44f43e20123566affaad1fbdeed37b61356
Noman Rafiq <heyimnomee@gmail.com>
2024-01-09 23:53:07 +0500

    async await promises try catch

--- asynchronous-programming-with-javascript/4- Exploring Async-Await/2-convert-promise-handling-to-async-await/start/js/async-await.js
+++ asynchronous-programming-with-javascript/4- Exploring Async-Await/2-convert-promise-handling-to-async-await/start/js/async-await.js
@@ -3,15 +3,23 @@ const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
+async function getJSON(url) {
+    try {
+        const response = await fetch(url);
+        return await response.json();
+        // try will contain all the code that need to be executed
+    } catch (err) {
+        throw err;
+        // will be executed if the exception is thrown
+    }
+}
 // Handle all fetch requests
 async function getPeopleInSpace(url) {
-    const peopleResponse = await fetch(url);
-    const peopleJson = await peopleResponse.json();
+    const peopleJson = await getJSON(url);
 
     const profiles = peopleJson.people.map(async (person) => {
         const craft = person.craft;
-        const profileRes = await fetch(wikiUrl + person.name);
-        const profileJson = await profileRes.json();
+        const profileJson = await getJSON(wikiUrl + person.name);
 
         return { ...profileJson, craft };
     });
@@ -47,5 +55,10 @@ btn.addEventListener('click', (event) => {
     event.target.textContent = 'Loading...';
     getPeopleInSpace(astrosUrl)
         .then(generateHTML)
+        .catch(
+            (error) =>
+                (peopleList.innerHTML = `<h3>Something went wrong</h3>`) +
+                console.error(error)
+        )
         .finally(() => event.target.remove());
 });
