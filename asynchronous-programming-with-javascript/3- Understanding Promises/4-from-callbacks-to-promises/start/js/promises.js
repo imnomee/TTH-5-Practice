10c3f3f07cbaa14021d7447e125d94b61d28187b
Noman Rafiq <heyimnomee@gmail.com>
2024-01-09 20:30:35 +0500

    xhr to promises

--- asynchronous-programming-with-javascript/3- Understanding Promises/4-from-callbacks-to-promises/start/js/promises.js
+++ asynchronous-programming-with-javascript/3- Understanding Promises/4-from-callbacks-to-promises/start/js/promises.js
@@ -3,47 +3,56 @@ const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
-function getJSON(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = () => {
-    if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      return callback(data);
-    }
-  };
-  xhr.send();
+function getJSON(url) {
+    return new Promise((res, rej) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                res(data);
+            } else {
+                rej(Error(xhr.statusText));
+            }
+        };
+        xhr.onerror = () => rej(Error('A network error'));
+        xhr.send();
+    });
 }
 
 function getProfiles(json) {
-  json.people.map( person => {
-    getJSON(wikiUrl + person.name, generateHTML);      
-  }); 
+    const profiles = json.people.map((person) => {
+        return getJSON(wikiUrl + person.name).then(generateHTML);
+    });
+    return profiles;
 }
 
 // Generate the markup for each profile
 function generateHTML(data) {
-  const section = document.createElement('section');
-  peopleList.appendChild(section);
-  // Check if request returns a 'standard' page from Wiki
-  if (data.type === 'standard') {
-    section.innerHTML = `
+    const section = document.createElement('section');
+    peopleList.appendChild(section);
+    // Check if request returns a 'standard' page from Wiki
+    if (data.type === 'standard') {
+        section.innerHTML = `
       <img src=${data.thumbnail.source}>
       <h2>${data.title}</h2>
       <p>${data.description}</p>
       <p>${data.extract}</p>
     `;
-  } else {
-    section.innerHTML = `
+    } else {
+        section.innerHTML = `
       <img src="img/profile.jpg" alt="ocean clouds seen from space">
       <h2>${data.title}</h2>
       <p>Results unavailable for ${data.title}</p>
       ${data.extract_html}
     `;
-  }
+    }
 }
 
 btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl, getProfiles);
-  event.target.remove();
+    getJSON(astrosUrl)
+        .then(getProfiles)
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+    event.target.remove();
 });
