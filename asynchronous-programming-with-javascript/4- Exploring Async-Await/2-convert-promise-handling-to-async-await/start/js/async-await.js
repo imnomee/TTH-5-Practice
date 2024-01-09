42f5fbf48bd20d7c749bf9943765998649a0a11d
Noman Rafiq <heyimnomee@gmail.com>
2024-01-09 23:38:40 +0500

    async await

--- asynchronous-programming-with-javascript/4- Exploring Async-Await/2-convert-promise-handling-to-async-await/start/js/async-await.js
+++ asynchronous-programming-with-javascript/4- Exploring Async-Await/2-convert-promise-handling-to-async-await/start/js/async-await.js
@@ -4,34 +4,48 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Handle all fetch requests
+async function getPeopleInSpace(url) {
+    const peopleResponse = await fetch(url);
+    const peopleJson = await peopleResponse.json();
 
+    const profiles = peopleJson.people.map(async (person) => {
+        const craft = person.craft;
+        const profileRes = await fetch(wikiUrl + person.name);
+        const profileJson = await profileRes.json();
+
+        return { ...profileJson, craft };
+    });
+    return Promise.all(profiles);
+}
 
 // Generate the markup for each profile
 function generateHTML(data) {
-  data.map( person => {
-    const section = document.createElement('section');
-    peopleList.appendChild(section);
-    // Check if request returns a 'standard' page from Wiki
-    if (person.type === 'standard') {
-      section.innerHTML = `
+    data.map((person) => {
+        const section = document.createElement('section');
+        peopleList.appendChild(section);
+        // Check if request returns a 'standard' page from Wiki
+        if (person.type === 'standard') {
+            section.innerHTML = `
         <img src=${person.thumbnail.source}>
         <span>${person.craft}</span>
         <h2>${person.title}</h2>
         <p>${person.description}</p>
         <p>${person.extract}</p>
       `;
-    } else {
-      section.innerHTML = `
+        } else {
+            section.innerHTML = `
         <img src="img/profile.jpg" alt="ocean clouds seen from space">
         <h2>${person.title}</h2>
         <p>Results unavailable for ${person.title}</p>
         ${person.extract_html}
       `;
-    }
-  });
+        }
+    });
 }
 
-btn.addEventListener('click', (event) => {
-  event.target.textContent = "Loading...";
-
+btn.addEventListener('click', async (event) => {
+    event.target.textContent = 'Loading...';
+    const astros = await getPeopleInSpace(astrosUrl);
+    generateHTML(astros);
+    event.target.remove();
 });
